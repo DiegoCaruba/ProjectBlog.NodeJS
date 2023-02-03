0ef190a27d1abbc02bf409da4cea5028e92d7e80
Diego Caruba <diego.caruba.carmo@ibm.com>
2023-02-03 09:19:12 -0300

    feat: criação serviços posts e comments

---
+++ comments/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = 4001;
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+  
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(PORT, () => {
+  console.log(`Comments app is running on http://localhost:${PORT}`);
+})
---
+++ posts/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = 4000;
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id, title
+  }
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Posts app is running on http://localhost:${PORT}`);
+})
