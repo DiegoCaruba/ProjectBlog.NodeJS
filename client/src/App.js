fff6268615999d6fad2a06a864796cdf990f8fa5
Diego Caruba <diego.caruba@gmail.com>
2023-02-06 16:48:50 -0300

    feat: adiciona lista de posts renderizados

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
-export default () => {
+const App = () => {
   return (
     <div className='container'>
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h1>Post</h1>
+      <PostList />
     </div>
   );
 }
+
+export default App;
