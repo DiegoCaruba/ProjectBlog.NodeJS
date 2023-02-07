7b413d44d4ae13495c10a170888905d671945ac6
Diego Caruba <diego.caruba@gmail.com>
2023-02-07 16:28:59 -0300

    feat: criação do serviço query

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,16 +7,16 @@ const PORT = 4005;
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event)
+    await axios.post('http://localhost:4000/events', event)
         .catch((err) => { console.log(err.message) });
 
-    axios.post('http://localhost:4001/events', event)
+    await axios.post('http://localhost:4001/events', event)
         .catch((err) => { console.log(err.message) });
 
-    axios.post('http://localhost:4002/events', event)
+    await axios.post('http://localhost:4002/events', event)
         .catch((err) => { console.log(err.message) });
     
     res.send({ status: 'OK' });
